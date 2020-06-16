c08728f11c8112c661ced708a56eb4297ad2fcb1
donovanherion <donovanherion@gmail.com>
2020-06-16 17:01:27 +0200

    first page

--- assets/js/character.js
+++ assets/js/character.js
@@ -1,3 +1,55 @@
+//Character choice picture update
+
+const leftSection = document.querySelector(".left")
+const rightSection = document.querySelector(".right")
+
+const chosenCharacter1 = document.getElementById('liste-persos1')
+const chosenCharacter2 = document.getElementById('liste-persos2')
+
+chosenCharacter1.addEventListener("change", () => {
+    switch (chosenCharacter1.value) {
+        case 'Sangoku':
+            leftSection.style.backgroundImage = 'url(../assets/images/sangoku.png)'
+            break;
+        case 'Vegeta':
+            leftSection.style.backgroundImage = 'url(../assets/images/vegeta.png)'
+            break;
+        case 'Picollo':
+            leftSection.style.backgroundImage = 'url(../assets/images/picollo.png)'
+            break;
+        case 'Freezer':
+            leftSection.style.backgroundImage = 'url(../assets/images/freezer.png)'
+            break;
+            
+            default:
+                leftSection.style.backgroundImage = 'url(../assets/images/wireframe.png)'
+                break;
+            }
+    })
+
+chosenCharacter2.addEventListener("change", () => {
+    switch (chosenCharacter2.value) {
+        case 'Sangoku':
+            rightSection.style.backgroundImage = 'url(../assets/images/sangoku.png)'
+            break;
+        case 'Vegeta':
+            rightSection.style.backgroundImage = 'url(../assets/images/vegeta.png)'
+            break;
+        case 'Picollo':
+            rightSection.style.backgroundImage = 'url(../assets/images/picollo.png)'
+            break;
+        case 'Freezer':
+            rightSection.style.backgroundImage = 'url(../assets/images/freezer.png)'
+            break;
+            
+            default:
+                rightSection.style.backgroundImage = 'url(../assets/images/wireframe.png)'
+                break;
+            }
+    })
+
+
+
 //Use this script to generate your character
 function Person(race,item){
     this.race = race;
