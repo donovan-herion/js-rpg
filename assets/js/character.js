7b7841589fbe5308d0275e4974b233a91cf99676
becode <mehdi.semlali.1988@gmail.com>
2020-06-16 17:01:51 +0200

    debut fonction creation perso

--- assets/js/character.js
+++ assets/js/character.js
@@ -1,21 +1,22 @@
 //Use this script to generate your character
-function Person(race,item){
-    this.race = race;
-    this.item = item;
+function Person(heroes, power) {
+    this.heroes = heroes;
+    this.power = power;
     this.currenthealth = 100;
     this.maxHealth = 100;
-    
+
     this.min = 3;
     this.maxDamage = 20;
     this.maxHealing = 30;
 
-    this.heal = function(){};
+    this.heal = function () { };
 
-    this.damage = function(){};
+    this.damage = function () { };
 
     this.totalDamage = this.damage();
 
-    this.displayChar = function(){
-        return console.log(`I am a ${this.race}, I wield a ${this.item}, my total health point are ${this.maxHealth}`);
+    this.displayChar = function () {
+        return console.log(`I am a ${this.heroes}, I wield a ${this.power}, my total health point are ${this.maxHealth}`);
     };
 }
+console.log(Person);
---
+++ assets/js/gamelogic.js
@@ -0,0 +1,19 @@
+
+
+
+
+document.getElementById("run").addEventListener("click", create);
+
+
+function create() {
+fighter1Heroes = document.getElementById("liste-perso1").value;
+fighter1Power = document.getElementById("liste-pouvoir1").value;
+fighter2Heroes = document.getElementById("liste-perso2").value;
+fighter2Power = document.getElementById("liste-pouvoir2").value;
+
+
+    console.log(fighter1Heroes);
+    console.log(fighter1Power);
+    console.log(fighter2Heroes);
+    console.log(fighter2Power);
+}
