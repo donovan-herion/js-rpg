1808f077b27295cf5464d0d0f50b2d7e8d608447
becode <mehdi.semlali.1988@gmail.com>
2020-06-17 09:17:47 +0200

    preparation de la fusion des branches vers master

--- assets/js/character.js
+++ assets/js/character.js
@@ -1,3 +1,55 @@
+//Character choice picture update
+
+const leftSection = document.querySelector(".left")
+const rightSection = document.querySelector(".right")
+
+const chosenCharacter1 = document.getElementById('liste-persos1')
+const chosenCharacter2 = document.getElementById('liste-persos2')
+
+chosenCharacter1.addEventListener("change", () => {
+    switch (chosenCharacter1.value) {
+        case 'Sangoku':
+            leftSection.style.backgroundImage = 'url(../assets/images/sangoku.png)'
+            break;
+        case 'Vegeta':
+            leftSection.style.backgroundImage = 'url(../assets/images/vegeta.png)'
+            break;
+        case 'Picollo':
+            leftSection.style.backgroundImage = 'url(../assets/images/picollo.png)'
+            break;
+        case 'Freezer':
+            leftSection.style.backgroundImage = 'url(../assets/images/freezer.png)'
+            break;
+            
+            default:
+                leftSection.style.backgroundImage = 'url(../assets/images/wireframe.png)'
+                break;
+            }
+    })
+
+chosenCharacter2.addEventListener("change", () => {
+    switch (chosenCharacter2.value) {
+        case 'Sangoku':
+            rightSection.style.backgroundImage = 'url(../assets/images/sangoku.png)'
+            break;
+        case 'Vegeta':
+            rightSection.style.backgroundImage = 'url(../assets/images/vegeta.png)'
+            break;
+        case 'Picollo':
+            rightSection.style.backgroundImage = 'url(../assets/images/picollo.png)'
+            break;
+        case 'Freezer':
+            rightSection.style.backgroundImage = 'url(../assets/images/freezer.png)'
+            break;
+            
+            default:
+                rightSection.style.backgroundImage = 'url(../assets/images/wireframe.png)'
+                break;
+            }
+    })
+
+
+
 //Use this script to generate your character
 function Person(heroes, power) {
     this.heroes = heroes;
@@ -20,3 +72,48 @@ function Person(heroes, power) {
     };
 }
 console.log(Person);
+
+// (() => {
+
+//     class Personage {
+//         constructor(firstname){
+//             this.firstname = firstname;
+        
+//         }
+    
+//         set name(text){
+//             let arr_nam = text.split(" ");
+//             this.firstname = arr_nam[0];
+           
+//         }
+//         get name(){
+//             return `${this.firstname} `;
+//         }
+//     }
+   
+//         let goku = new Personage("Goku")
+       
+//         console.log(goku.name);
+    
+//         goku.name = "Songoku";
+//         console.log(goku.name);
+   
+    
+//     })();
+
+
+
+//     const perso = {
+//         set current(name) {
+//           this.log.push(name);
+//         },
+//         log: []
+//       };
+      
+//       perso.current = 'Songoku';
+//       perso.current = 'Vegeta';
+//       perso.current = 'Frezzer';
+//       perso.current = 'Picoolo';
+      
+//       console.log(perso.log);
+     
--- assets/js/gamelogic.js
+++ assets/js/gamelogic.js
@@ -11,7 +11,6 @@ fighter1Power = document.getElementById("liste-pouvoir1").value;
 fighter2Heroes = document.getElementById("liste-perso2").value;
 fighter2Power = document.getElementById("liste-pouvoir2").value;
 
-
     console.log(fighter1Heroes);
     console.log(fighter1Power);
     console.log(fighter2Heroes);
